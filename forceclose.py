7e08dab0c12e9873f0c52a7d096f29eb493ad326
Jia Jie Lee <LEEJ438492@exactsoftware.com>
2024-06-10 11:51:41 +0800

    Completed copy and paste updated files Completed E6Shell launching Added gitignore

--- forceclose.py
+++ forceclose.py
@@ -1,29 +1,72 @@
 import os
+import threading
 import time
+import shutil
+import pathway
+from datetime import datetime
 import subprocess
+import watchdog.events
+import watchdog.observers
 
-def monitor_folder(folder_path, program_to_close):
-    # Get the initial modification time of the folder
-    initial_mod_time = os.path.getmtime(folder_path)
 
-    while True:
-        # Check if the folder has been modified
-        current_mod_time = os.path.getmtime(folder_path)
-        if current_mod_time > initial_mod_time:
+class FileModifiedHandler(watchdog.events.FileSystemEventHandler):
+    
+    def __init__(self, program_to_close):
+        self.last_modified = datetime.now()
+        self.program_to_close = program_to_close
+        self.timer_started = False
+        
+    def start_timer(self):
+        self.timer_started = True
+        threading.Timer(5, self.launch_program).start()
+
+    def launch_program(self):
+        subprocess.run([pathway.exe_path])
+        self.timer_started = False
+        
+    def on_modified(self, event):
+        # if datetime.now() - self.last_modified < timedelta(seconds=1):
+        #     subprocess.call(['taskkill', '/F', '/IM', self.program_to_close])
+        # else:
+        #     self.last_modified = datetime.now()
+        
+        if not self.timer_started :
+            self.start_timer()
+
+        subprocess.call(['taskkill', '/F', '/IM', self.program_to_close])   
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        print(event.is_directory) # This attribute is also available
+        if not event.is_directory:
             # Force close the program
-            subprocess.call(['taskkill', '/F', '/IM', program_to_close])
+            #subprocess.call(['taskkill', '/F', '/IM', self.program_to_close])
+            print(f'event type: {event.event_type}  path : {event.src_path}')
+            # Get the file name from the event source path
+            file_name = os.path.basename(event.src_path)
+            print(f'File name: {file_name}')
+            shutil.copyfile(event.src_path, pathway.dst_path + '\\' + file_name)
+            # subprocess.run([pathway.exe_path])
 
-            # Update the initial modification time
-            initial_mod_time = current_mod_time
+def monitor_folder():
+    # Create a watchdog observer
+    observer = watchdog.observers.Observer()
 
-        # Wait for a certain interval before checking again
-        time.sleep(1)
+    # Create a file modified event handler
+    event_handler = FileModifiedHandler(pathway.close_path)
 
-# Specify the folder path to monitor
-folder_path = 'C:\\Users\\ENGS437957\\source\\repos\\Globe.NetApp\\binGlobe.Net'
+    # Schedule the event handler to monitor the folder
+    observer.schedule(event_handler, pathway.src_path, recursive=True)
 
-# Specify the program name to force close
-program_to_close = 'E6Shell.exe'
+    # Start the observer
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
 
 # Start monitoring the folder
-monitor_folder(folder_path, program_to_close)
+monitor_folder()
