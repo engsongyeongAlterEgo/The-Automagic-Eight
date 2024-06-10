adf01cef4829d84ec6d5c3e5eb849a4db9f77647
Jia Jie Lee <LEEJ438492@exactsoftware.com>
2024-06-10 14:16:09 +0800

    Upload example pathway.py

---
+++ pathway.py
@@ -0,0 +1,5 @@
+# Modify according to Machine and Exact Globe+ installation pathway
+src_path = "C:\\Users\\leej438492\\source\\repos\\Globe.NetApp\\binGlobe.Net"
+dst_path = "C:\\Program Files (x86)\\Exact Globe+\\bin"
+exe_path = "C:\\Program Files (x86)\\Exact Globe+\\bin\\E6Shell.exe"
+close_path = "E6Shell.exe"
