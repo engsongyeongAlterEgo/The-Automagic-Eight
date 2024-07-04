11d3a73f3c5a80836c9120e4076dd2ebbfb6832d
Song Yeong Eng <ENGS437957@exactsoftware.com>
2024-07-04 13:45:53 +0800

    able to click weekly

--- hourEntry.py
+++ hourEntry.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Initialize the WebDriver for Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -32,4 +33,21 @@ password_field.send_keys("")# put password
 # Find the button by its ID and click it
 submit_button = driver.find_element(By.ID, "LoginForm_btnSave")
 submit_button.click()
+
+# Wait for the "Entries" menu item to be clickable and then click it
+entries_menu_item = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "Entries"))
+)
+ActionChains(driver).move_to_element(entries_menu_item).click().perform()
+
+# Wait for the "Weekly" link to be clickable and then click it
+weekly_link = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "Entries_Entries_Entries_Weekly"))
+)
+weekly_link.click()
+
+project_field = driver.find_element(By.ID, "mtx_r0_Account_alt")
+project_field.send_keys("54")  # Replace "54" with the actual value you want to input
+
+
 input("Press Enter to close the browser...")
